d7dde2b8be0dd40c847b9f0be97a9f0dc74aa4d1
shankar jeve <shankarjeve900@gmail.com>
2026-04-21 10:30:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,13 @@
 import boto3
 
-# Your S3 bucket name
 bucket_name = "shankar-bucket-21-4"
 
-# Create S3 client
-s3 = boto3.client(
-    's3',
-    aws_access_key_id='YOUR_ACCESS_KEY',
-    aws_secret_access_key='YOUR_SECRET_KEY'
-)
+s3 = boto3.client('s3')
 
-# File to upload
 file_name = "index.html"
 
 try:
     s3.upload_file(file_name, bucket_name, file_name)
-    print("File uploaded successfully to S3")
+    print("File uploaded successfully")
 except Exception as e:
-    print("Error uploading file:", e)
+    print("Error:", e)
